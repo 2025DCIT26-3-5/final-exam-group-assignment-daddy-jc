be90b1de69f30b9f302a81ae0d7cf065b3d0d6aa
Rodriguez747 <ahronchristian.rodriguez@cvsu.edu.ph>
2026-01-14 08:22:30 +0800

    Push all local changes to new remote repository

---
+++ app/src/config/api.ts
@@ -0,0 +1,6 @@
+import Constants from 'expo-constants';
+import { Platform } from 'react-native';
+
+// Always use deployed Render backend for all platforms
+export const API_URL = 'https://emergencyresponse-n8cc.onrender.com/api';
+export default API_URL;
